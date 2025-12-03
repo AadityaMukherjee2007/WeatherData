e6f0a448cf6b367ce4711fa4a55170e3f70168dc
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-12-03 12:43:26 +0530

    esp code added

---
+++ esp_weather_data/esp_weather_data.ino
@@ -0,0 +1,85 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+
+const char* WIFI_SSID = "i=q/t#Current";
+const char* WIFI_PASS = "2022#ElectronFlow";
+
+const char* MQTT_SERVER = "192.168.0.191"; 
+const int MQTT_PORT = 1883;
+const char* MQTT_TOPIC = "sensor/data";
+
+const unsigned long PUBLISH_INTERVAL = 5000;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastPublish = 0;
+
+
+void setupWiFi() {
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void mqttReconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected!");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 2 seconds");
+      delay(2000);
+    }
+  }
+}
+
+
+void publishData() {
+  StaticJsonDocument<100> doc;
+
+  doc["data"] = random(0, 1000);
+
+  char buffer[100];
+  serializeJson(doc, buffer);
+
+  client.publish(MQTT_TOPIC, buffer);
+  Serial.print("Published: ");
+  Serial.println(buffer);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  setupWiFi();
+
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+}
+
+
+void loop() {
+  if (!client.connected()) {
+    mqttReconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastPublish >= PUBLISH_INTERVAL) {
+    lastPublish = now;
+    publishData();
+  }
+}
