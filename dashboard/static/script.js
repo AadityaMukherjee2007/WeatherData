6d5a72093688704b6a0d8d94c575ad83a9350af3
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-12-11 12:36:42 +0530

    frontend update with new weather parameters

--- dashboard/static/script.js
+++ dashboard/static/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     updateValues();
+    dataDescription();
 
     setInterval(displayTime, 1000);
     setInterval(updateValues, 480000); // update values after 7 mins
@@ -14,6 +15,9 @@ function displayTime() {
 
 function updateValues() {
     const temp_val = document.getElementById('temp-val');
+    const HI_val = document.getElementById('HI-val');
+    const humidity_val = document.getElementById('humidity-val');
+    const dewpoint_val = document.getElementById('dewpoint-val');
     const atm_val = document.getElementById('atm-val');
 
     fetch('/getCurrentData')
@@ -22,7 +26,33 @@ function updateValues() {
         console.log(data);
         
         temp_val.textContent = `${data['temp_c']}°C`;
+        HI_val.textContent = `${data['heatIndex_c']}°C`
+        humidity_val.textContent = `${data['humidity']}°C`
+        dewpoint_val.textContent = `${data['dewPoint_c']}°C`
         atm_val.textContent = `${data['pressure']} hPa`;
     })
     .catch(error => console.error(error));
+}
+
+function dataDescription() {
+    const tooltip = document.getElementById("tooltip");
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        card.addEventListener("mouseenter", () => {
+            tooltip.textContent = card.dataset.description;
+            tooltip.style.opacity = 1;
+        });
+
+        card.addEventListener("mousemove", (e) => {
+            const offsetX = 60;  
+            const offsetY = 60;  
+            tooltip.style.left = `${e.clientX + offsetX}px`;
+            tooltip.style.top = `${e.clientY + offsetY}px`;
+        });
+
+        card.addEventListener("mouseleave", () => {
+            tooltip.style.opacity = 0;
+        });
+    });
 }
